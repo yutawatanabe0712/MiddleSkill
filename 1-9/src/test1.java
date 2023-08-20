a3ec13cb0b7785907e1302d0ffe1c8f8742f8b14
yutawatanabe0712 <yutaoneok0712@icloud.com>
2023-08-20 15:53:09 +0900

    課題1-9提出

---
+++ 1-9/src/test1.java
@@ -0,0 +1,17 @@
+public class test1 {
+	public static void main(String[] args) {
+
+//		1.数字の5と9の変数を定義し、足した数字を出力しなさい。
+		int number1 = 5;
+		int number2 = 9;
+		System.out.println(number1 + number2);
+
+//		2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
+		long number3 = 1111111111L;
+		System.out.println(number3 * 2);
+
+//		3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。
+		boolean b = 5 == 4;
+		System.out.println(b);
+	}
+}
