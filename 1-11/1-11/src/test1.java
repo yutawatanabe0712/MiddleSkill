7095de5306ba1bc81c0a962ab7b6f386fa91987e
yutawatanabe0712 <yutaoneok0712@icloud.com>
2023-08-26 12:10:16 +0900

    課題1-11修正
    問２をif文→三項演算子へ変更
    
    問4をresults２つ→１つへ変更

--- 1-11/1-11/src/test1.java
+++ 1-11/1-11/src/test1.java
@@ -5,7 +5,7 @@ public class test1 {
 		//問題1:変数"numa"にインクリメント演算子を利用し1マイナスしたものを表示してください。
 
 		int numa = 5;
-		numa --;
+		numa--;
 		System.out.println(numa);
 
 		//改行
@@ -15,45 +15,35 @@ public class test1 {
 
 		int numb = 5;
 		String result = "";
-		if(numb > 0) {
-			result = "positive";
-		}else if(numb < 0) {
-			result = "negative";
-		}else {
-			result = "zero";
-		}
+		result = numb > 0 ? "positive" : numb < 0 ? "negative" : "etc";
 		System.out.println(result);
-//改行
-	System.out.println("問３");
-//問題3:"numc","numd"2つの変数の大きい方の値を出力するプログラムを三項演算子を用いて作成してください。
-//(int intResult変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
-	//※※必ず全パターン正常動作することをコンソールで確認して下さい。
 
-int numc = 1;
-int numd = 2;
-int intResult = 0;
-intResult = numc > numd? numc : numd;
-System.out.println(intResult);
+		//改行
+		System.out.println("問３");
+		//問題3:"numc","numd"2つの変数の大きい方の値を出力するプログラムを三項演算子を用いて作成してください。
+		//(int intResult変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
+		//※※必ず全パターン正常動作することをコンソールで確認して下さい。
 
+		int numc = 1;
+		int numd = 2;
+		int intResult = 0;
+		intResult = numc > numd ? numc : numd;
+		System.out.println(intResult);
 
-//改行
-	System.out.println("問４");
-//問題4:"nume"と"numf"の変数が両方の正の場合は"both positive"、どちらかの値が正の場合は"one positive"、
-//両方の値が負の場合は"both negative"、それ以外は"etc"と出力するプログラムを三項演算子を用いて作成してください。
-//(String results変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
-//※※必ず全パターン正常動作することをコンソールで確認して下さい。
-int nume = 4;
-int numf = -3;
-String results = "";
-//両方正→bouth, nume正→one, numf正→etc, 両方負→etc
-results = (nume > 0)?(numf > 0)? "both positive" : "one positive" : "etc" ;
-//numf正→etcの場合のみnumf→oneに修正
-results = (numf > 0)?(results == "etc")? "one positive" : results : results;
-System.out.println(results);
+		//改行
+		System.out.println("問４");
+		//問題4:"nume"と"numf"の変数が両方の正の場合は"both positive"、どちらかの値が正の場合は"one positive"、
+		//両方の値が負の場合は"both negative"、それ以外は"etc"と出力するプログラムを三項演算子を用いて作成してください。
+		//(String results変数を用いてそこに値を格納しそれを出力する形式でお願いいたします)
+		//※※必ず全パターン正常動作することをコンソールで確認して下さい。
+		int nume = 4;
+		int numf = -3;
+		String results = "";
+		results =
+			(nume > 0)&&(numf > 0)? "both positive" :
+			(nume > 0)&&(numf < 0)? "one positive" :
+			(nume < 0)&&(numf > 0)? "one positive" : "etc";
+		System.out.println(results);
 
 	}
 }
-
-
-
-
