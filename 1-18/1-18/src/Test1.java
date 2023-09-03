040581e3b0307f2b598ab66e385547280a98b4df
yutawatanabe0712 <yutaoneok0712@icloud.com>
2023-09-03 19:36:51 +0900

    課題1-18提出

---
+++ 1-18/1-18/src/Test1.java
@@ -0,0 +1,57 @@
+public class Test1 {
+	//問題1:上記変数のgetterとsetterメソッドを作成してください。※IDE（Eclipse）で自動生成で作成すること
+	//問題2:RTC終了後の個人またはチーム開発ではgetterメソッドとsetterメソッドを変数に対して作成していないと思われますがですがそれはなぜですか？
+	//回答2「　Lombokのライブラリよりアノテーションを使用して省略したため。　」
+	//問題3:getterメソッドとsetterメソッドをカプセル化しようとする時どのようなアクセス修飾子が適切ですか？
+	//回答3「　フィールドにはprivate、メソッドにはpublic。　」
+
+	private String first_name;
+
+	private String last_name;
+
+	private String name;
+
+	private int age;
+
+	private int weight;
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+}
